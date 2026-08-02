fc3703c014e4fbb5df350af472f1455fc30c0073
khalifahnur <khalifahnur1095@gmail.com>
2026-08-02 17:20:05 +0300

    feat:added cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -41,11 +41,27 @@ func main() {
 
 	corsMiddleware := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			origin := r.Header.Get("Origin")
+			allowedOrigins := []string{
+				"https://karena.stsebastiansportsacademy.com",
+				//"http://localhost:3000",
+			}
 
-			if r.Method == "OPTIONS" {
+			isAllowed := false
+			for _, o := range allowedOrigins {
+				if origin == o {
+					isAllowed = true
+					break
+				}
+			}
+			if isAllowed {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			}
+
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			if r.Method == http.MethodOptions {
 				w.WriteHeader(http.StatusOK)
 				return
 			}

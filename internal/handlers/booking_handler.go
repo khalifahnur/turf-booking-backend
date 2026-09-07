13711cb051f69b791fb4ad59c9459e5db58a804b
khalifahnur <khalifahnur1095@gmail.com>
2026-09-07 11:45:31 +0300

    ref:changed the amount for testing

--- internal/handlers/booking_handler.go
+++ internal/handlers/booking_handler.go
@@ -197,10 +197,10 @@ func (h *BookingHandler) InitiateBooking(w http.ResponseWriter, r *http.Request)
 
 	switch req.PitchType {
 	case "8Aside":
-		amountInCents = 12000 * 100
+		amountInCents = 1 * 100
 		requiredCapacity = 2
 	case "5Aside":
-		amountInCents = 6500 * 100
+		amountInCents = 1 * 100
 		requiredCapacity = 1
 	default:
 		http.Error(w, "Invalid pitch type provided", http.StatusBadRequest)
